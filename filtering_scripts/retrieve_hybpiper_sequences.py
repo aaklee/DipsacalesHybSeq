fa67c390964bb31b6db61d1df30b69838da6aed7
aaklee <leea33@tcnj.edu>
2020-06-26 12:54:34 -0400

    minor changes

--- filtering_scripts/retrieve_hybpiper_sequences.py
+++ filtering_scripts/retrieve_hybpiper_sequences.py
@@ -11,7 +11,7 @@ import os
 import argparse
 from Bio import SeqIO
 
-def retrieve_sequences(sampledir, taxon_pool, STEP, convert_names): # shamelessly copied & modified from hybpiper/retrieve_sequences.py
+def retrieve_sequences(sampledir, target_file, taxon_pool, convert_names, exclude_paralogs): # shamelessly copied & modified from hybpiper/retrieve_sequences.py
     retrieve = ['dna', 'intron', 'supercontig', 'aa']
     for r in retrieve:
         if r == 'dna':
@@ -25,20 +25,24 @@ def retrieve_sequences(sampledir, taxon_pool, STEP, convert_names): # shamelessl
         elif r == 'aa':
             seq_dir = 'FAA'
 
-        #Use gene names parsed from a bait file.
-        baitfile = os.path.abspath('/projects/clement-lab/5-HybSeq/2-hybpiper_Dipsacales/Dipsacales_Baits_Project/Angiosperms353_targetSequences.fasta')
+        # Use gene names parsed from a bait file.
+        baitfile = target_file
         target_genes_dict = SeqIO.to_dict(SeqIO.parse(baitfile,'fasta'))
         target_genes = list(set([x.id.split('-')[-1] for x in SeqIO.parse(baitfile,'fasta')]))
 
         sample_names = [x for x in os.listdir(sampledir) if (os.path.isdir(os.path.join(sampledir,x)) and not x.startswith('.') and x in taxon_pool)]
 
-        paralogf = os.path.join(sampledir,'all_genes_with_paralog_warnings.txt')
+        # check if exclude paralogs
         paralogs = []
-        with open(paralogf, 'r') as inf:
-            for line in inf:
-                paralogs.append(line.strip())
+        if exclude_paralogs:
+            paralogf = os.path.join(sampledir,'all_genes_with_paralog_warnings.txt')
+            with open(paralogf, 'r') as inf:
+                for line in inf:
+                    paralogs.append(line.strip())
+            print("Retrieving {} - {} (paralogs) genes from {} samples".format(len(target_genes), len(paralogs), len(sample_names)))
+        else:
+            print("Retrieving {} genes from {} samples".format(len(target_genes), len(sample_names)))
 
-        print("Retrieving {} - {} (paralogs) genes from {} samples".format(len(target_genes), len(paralogs), len(sample_names)))
 
         gene_count = 0
         sample_count = []
@@ -57,9 +61,11 @@ def retrieve_sequences(sampledir, taxon_pool, STEP, convert_names): # shamelessl
                     else:
                         sample_path = os.path.join(sampledir, sample, gene, sample, 'sequences', seq_dir, gene+'.'+seq_dir)
 
+
                     if os.path.isfile(sample_path):
                         for record in SeqIO.parse(sample_path, 'fasta'):
-                            record.id = convert_names[record.id[0:10]]
+                            if convert_names:
+                                record.id = convert_names[record.id[0:10]]
                             record.description = ''
                             gene_seqs.append(record)
                             sample_count.append(record.id)
@@ -76,14 +82,19 @@ def retrieve_sequences(sampledir, taxon_pool, STEP, convert_names): # shamelessl
                     SeqIO.write(gene_seqs, open(outfilename,'w'), 'fasta')
                     gene_count += 1
 
-        if r == 'dna':
-            os.system('mv *.FNA {}-hybpiper_sequences/dna'.format(STEP))
-        elif r == 'intron':
-            os.system('mv *.fasta {}-hybpiper_sequences/intron'.format(STEP))
-        elif r == 'supercontig':
-            os.system('mv *.fasta {}-hybpiper_sequences/supercontig'.format(STEP))
-        elif r == 'aa':
-            os.system('mv *.FAA {}-hybpiper_sequences/aa'.format(STEP))
+        if gene_seqs:
+            if r == 'dna':
+                os.system('mkdir 1-hybpiper_sequences/dna')
+                os.system('mv *.FNA 1-hybpiper_sequences/dna')
+            elif r == 'intron':
+                os.system('mkdir 1-hybpiper_sequences/intron')
+                os.system('mv *.fasta 1-hybpiper_sequences/intron')
+            elif r == 'supercontig':
+                os.system('mkdir 1-hybpiper_sequences/supercontig')
+                os.system('mv *.fasta 1-hybpiper_sequences/supercontig')
+            elif r == 'aa':
+                os.system('mkdir 1-hybpiper_sequences/aa')
+                os.system('mv *.FAA 1-hybpiper_sequences/aa')
 
         print('\n{} sequences recovered: {}'.format(r, gene_count))
         print('{} samples recovered: {}\n'.format(len(set(sample_count)), ','.join(list(set(sample_count)))))
@@ -95,20 +106,21 @@ def retrieve_sequences(sampledir, taxon_pool, STEP, convert_names): # shamelessl
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-hy', '--hybpiper', help='path to hybpiper output directory', required=True)
+    parser.add_argument('-tf', '--target_file', help='path to hybpiper target sequences', required=True)
     parser.add_argument('-ta', '--taxon_filtering', help='path to list of taxa to include or exclude, if None then assume to include all taxa')
     parser.add_argument('-tm', '--taxon_filtering_mode', help='\"include\" or \"exclude\" taxon filtering mode')
     #parser.add_argument('-ll', '--locus_length', action='store_true', help='include to filter by locus length')
     #parser.add_argument('-ml', '--missing_loci', action='store_true', help='include to produce trees/alignments combining missing loci (supercontig + exon)')
-    parser.add_argument('-rt', '--rogue_taxa', action='store_true', help='include to filter out rogue_taxa')
-    parser.add_argument('-ar', '--all_rogue_taxa', action='store_true', help='include to include ALL rogue taxa')
-
+    parser.add_argument('-rt', '--rogue_taxa', action='store_true', help='filter out any rogue_taxa')
+    parser.add_argument('-ar', '--all_rogue_taxa', action='store_true', help='include ALL rogue taxa')
+    parser.add_argument('-co', '--convert', action='store_true', help='convert sample names from \"Gilman\" code')
+    parser.add_argument('-ep', '--exclude_paralogs', action='store_true', help='exclude potential paralogs flagged by HybPiper')
     if len(sys.argv) <= 1:
         parser.print_help()
         sys.exit()
 
     args = parser.parse_args()
     print(args)
-    STEP = 1
 
     ##### RETRIEVE SEQEUENCES
     ####################################################################################################################
@@ -121,33 +133,51 @@ def main():
     exclude_taxa = []
     rogue_taxa = []
 
-    with open(taxonnames, 'r') as inf:
-        for line in inf:
-            all_taxa.append(line.strip().split(',')[0])
-            convert_names[line.strip().split(',')[0]] = line.strip().split(',')[1]
-            names_convert[line.strip().split(',')[1]] = line.strip().split(',')[0]
+    # if convert, get conversions
+    if args.convert:
+        with open(taxonnames, 'r') as inf:
+            for line in inf:
+                all_taxa.append(line.strip().split(',')[0])
+                convert_names[line.strip().split(',')[0]] = line.strip().split(',')[1]
+                names_convert[line.strip().split(',')[1]] = line.strip().split(',')[0]
+    else:
+        with open(os.path.join(os.path.abspath(args.hybpiper), 'reads_list.txt'), 'r') as inf:
+            for line in inf:
+                all_taxa.append(line.strip().split(' ')[0].split('/')[-1].split('_')[0])
 
+    # if rogue taxa, get rogue taxa
     if args.rogue_taxa:
         with open(rogue_taxa_f, 'r') as inf:
             for line in inf:
                 rogue_taxa.append(line.strip().split(',')[0])
-                
         exclude_taxa.extend(rogue_taxa)
 
+    # do not filter taxa
     if not args.taxon_filtering:
-        pass
+        samples = []
+        with open(os.path.join(os.path.abspath(args.hybpiper), 'reads_list.txt'), 'r') as inf:
+            for line in inf:
+                samples.append(line.strip().split(' ')[0].split('/')[-1].split('_')[0])
+
+        include_taxa = [i for i in samples]
+
+
+    # filter taxa
     else:
         with open(os.path.abspath(args.taxon_filtering), 'r') as inf:
             if args.taxon_filtering_mode == 'include':
                 for line in inf:
                     taxon = line.strip()
-                    if names_convert[taxon] not in include_taxa:
+                    if args.convert and names_convert[taxon] not in include_taxa:
                         include_taxa.append(names_convert[taxon])
+
+                    elif not args.convert and taxon not in include_taxa:
+                        include_taxa.append(taxon)
 
             elif args.taxon_filtering_mode == 'exclude':
                 for line in inf:
                     taxon = line.strip()
-                    if names_convert[taxon] not in exclude_taxa:
+                    if args.convert and names_convert[taxon] not in exclude_taxa:
                         if names_convert[taxon] in rogue_taxa and not args.all_rogue_taxa:
                             exclude_taxa.append(names_convert[taxon])
                         elif names_convert[taxon] in rogue_taxa and args.all_rogue_taxa:
@@ -155,11 +185,21 @@ def main():
                         elif names_convert[taxon] not in rogue_taxa:
                             exclude_taxa.append(names_convert[taxon])
 
+                    elif not args.convert and taxon not in exclude_taxa:
+                        if taxon in rogue_taxa and not args.all_rogue_taxa:
+                            exclude_taxa.append(taxon)
+                        elif taxon in rogue_taxa and args.all_rogueTaxa:
+                            pass
+                        elif taxon not in rogue_taxa:
+                            exclude_taxa.append(taxon)
+
     #if args.all_rogue_taxa:
     #    with open(rogue_taxa, 'r') as inf:
     #        for line in inf:
     #            if line.strip().split(',')[0] not in include_taxa:
     #                include_taxa.append(line.strip().split(',')[0])
+
+
 
     taxon_pool = []
     if not include_taxa:
@@ -167,27 +207,8 @@ def main():
     else:
         taxon_pool = [i for i in all_taxa if i in include_taxa and i not in exclude_taxa]
 
-
-    os.system('mkdir {}-hybpiper_sequences'.format(STEP))
-    os.system('mkdir {}-hybpiper_sequences/dna {}-hybpiper_sequences/intron {}-hybpiper_sequences/supercontig {}-hybpiper_sequences/aa'.format(STEP, STEP, STEP, STEP))
-    retrieve_sequences(os.path.abspath(args.hybpiper), taxon_pool, STEP, convert_names)
-
-    # FIXME: generate stats for how many taxa/loci per group, how many bases total
-
-    STEP += 1
-
-
-
-    ##### GUIDANCE FILTERING
-    ####################################################################################################################
-
-#    os.system('mkdir {}-guidance'.format(STEP))
-#    os.system('mkdir {}-guidance/dna'.format(STEP))
-#    os.system('mkdir {}-guidance/supercontig'.format(STEP))
-#    os.system('mkdir {}-guidance/intron'.format(STEP))
-#
-#    os.system()
-
+    os.system('mkdir 1-hybpiper_sequences')
+    retrieve_sequences(os.path.abspath(args.hybpiper), os.path.abspath(args.target_file), taxon_pool, convert_names, args.exclude_paralogs)
 
 
 
